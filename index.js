7c9c2bb3bed968057566b615106783d10c920807
tydillon <harrell_t@icloud.com>
2023-04-27 16:16:17 -0400

    troubleshooting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { google } = require('googleapis')
+// const { google } = require('googleapis')
 const sheets = google.sheets('v4');
 
 // export GOOGLE_APPLICATION_CREDENTIALS=./keys.json
@@ -15,46 +15,46 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-async function getAuthToken() {
-  const auth = new google.auth.GoogleAuth({
-    scopes: SCOPES
-  });
-  const authToken = await auth.getClient();
-  return authToken;
-}
+// async function getAuthToken() {
+//   const auth = new google.auth.GoogleAuth({
+//     scopes: SCOPES
+//   });
+//   const authToken = await auth.getClient();
+//   return authToken;
+// }
 
-async function getSpreadSheet({spreadsheetId, auth}) {
-    const res = await sheets.spreadsheets.get({
-        spreadsheetId,
-        auth,
-    });
-    return res;
-}
+// async function getSpreadSheet({spreadsheetId, auth}) {
+//     const res = await sheets.spreadsheets.get({
+//         spreadsheetId,
+//         auth,
+//     });
+//     return res;
+// }
 
-    async function getSpreadSheetValues({spreadsheetId, auth, sheetName, range}) {
-    const res = await sheets.spreadsheets.values.get({
-        spreadsheetId,
-        auth,
-        range: `${sheetName}!${range}`
-    });
-    return res;
-}
+//     async function getSpreadSheetValues({spreadsheetId, auth, sheetName, range}) {
+//     const res = await sheets.spreadsheets.values.get({
+//         spreadsheetId,
+//         auth,
+//         range: `${sheetName}!${range}`
+//     });
+//     return res;
+// }
 
 
-
-app.get("/api", async (req, res) => {
- const response = await getAuthToken()
-    .then(auth => getSpreadSheetValues({spreadsheetId, auth, sheetName, range}))
-    .then(res => res.data)
-    .then(data => data.values)
-    // .then(res => console.log({res: JSON.stringify(res.data)}))
-    .catch(err => console.log({err}))
-  console.log({response})
-  res.json(response);
-});
+// app.get("/api", async (req, res) => {
+//  const response = await getAuthToken()
+//     .then(auth => getSpreadSheetValues({spreadsheetId, auth, sheetName, range}))
+//     .then(res => res.data)
+//     .then(data => data.values)
+//     // .then(res => console.log({res: JSON.stringify(res.data)}))
+//     .catch(err => console.log({err}))
+//   console.log({response})
+//   res.json(response);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+  console.log("Spreadsheet ID: " + spreadsheetId)
 });
 
 // https://hackernoon.com/how-to-use-google-sheets-api-with-nodejs-cz3v316f
