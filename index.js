6bb37b27776b09e93bb2dce67499a014b5d0690f
tydillon <harrell_t@icloud.com>
2023-04-27 16:35:32 -0400

    the real deal

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const { google } = require('googleapis')
-// const sheets = google.sheets('v4');
+const { google } = require('googleapis')
+const sheets = google.sheets('v4');
 
 // export GOOGLE_APPLICATION_CREDENTIALS=./keys.json
 // export GCLOUD_PROJECT=tactile-cinema-384614
@@ -15,45 +15,45 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-// async function getAuthToken() {
-//   const auth = new google.auth.GoogleAuth({
-//     scopes: SCOPES
-//   });
-//   const authToken = await auth.getClient();
-//   return authToken;
-// }
+async function getAuthToken() {
+  const auth = new google.auth.GoogleAuth({
+    scopes: SCOPES
+  });
+  const authToken = await auth.getClient();
+  return authToken;
+}
 
-// async function getSpreadSheet({spreadsheetId, auth}) {
-//     const res = await sheets.spreadsheets.get({
-//         spreadsheetId,
-//         auth,
-//     });
-//     return res;
-// }
+async function getSpreadSheet({spreadsheetId, auth}) {
+    const res = await sheets.spreadsheets.get({
+        spreadsheetId,
+        auth,
+    });
+    return res;
+}
 
-//     async function getSpreadSheetValues({spreadsheetId, auth, sheetName, range}) {
-//     const res = await sheets.spreadsheets.values.get({
-//         spreadsheetId,
-//         auth,
-//         range: `${sheetName}!${range}`
-//     });
-//     return res;
-// }
+    async function getSpreadSheetValues({spreadsheetId, auth, sheetName, range}) {
+    const res = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        auth,
+        range: `${sheetName}!${range}`
+    });
+    return res;
+}
 
 
-// app.get("/api", async (req, res) => {
-//  const response = await getAuthToken()
-//     .then(auth => getSpreadSheetValues({spreadsheetId, auth, sheetName, range}))
-//     .then(res => res.data)
-//     .then(data => data.values)
-//     // .then(res => console.log({res: JSON.stringify(res.data)}))
-//     .catch(err => console.log({err}))
-//   console.log({response})
-//   res.json(response);
-// });
+app.get("/api", async (req, res) => {
+ const response = await getAuthToken()
+    .then(auth => getSpreadSheetValues({spreadsheetId, auth, sheetName, range}))
+    .then(res => res.data)
+    .then(data => data.values)
+    // .then(res => console.log({res: JSON.stringify(res.data)}))
+    .catch(err => console.log({err}))
+  console.log({response})
+  res.json(response);
+});
 
 app.get("/", async (req,res) => {
-  res.json("You did it!")
+  res.json("Server is healthy")
 })
 
 app.listen(PORT, () => {
